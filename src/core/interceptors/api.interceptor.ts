8c16efe1165863b813c774f0f894ef8d768ab6ad
qiujie <qiujieee_empty@outlook.com>
2023-05-24 17:22:36 +0800

    done 4 mj

--- src/core/interceptors/api.interceptor.ts
+++ src/core/interceptors/api.interceptor.ts
@@ -1,6 +1,14 @@
+/*
+ * @Author: qiujie qiujieee_empty@outlook.com
+ * @Date: 2023-05-22 21:36:22
+ * @LastEditors: qiujie qiujieee_empty@outlook.com
+ * @LastEditTime: 2023-05-23 21:00:46
+ * @FilePath: /yuque_hook/src/core/interceptors/api.interceptor.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { ErrorCode } from './../enmus/error-code.enum';
 import { ResponseResult } from './../entities/response-result';
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, StreamableFile } from '@nestjs/common';
+import { HttpStatus, Injectable, NestInterceptor, ExecutionContext, CallHandler, StreamableFile } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
 import { uuid } from 'short-uuid';
 
@@ -10,7 +18,10 @@ import { uuid } from 'short-uuid';
 @Injectable()
 export class APIInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest<Request>();
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse();
+
     // 设置TraceId
     const traceIdHeaderKey = 'traceid';
     let traceIdHeaderValue = request.headers[traceIdHeaderKey];
@@ -21,6 +32,12 @@ export class APIInterceptor implements NestInterceptor {
     }
     return next.handle().pipe(
       map((data) => {
+        if (request.method === 'POST') {
+          if (response.statusCode === HttpStatus.CREATED) {
+            response.status(HttpStatus.OK);
+          }
+        }
+
         if (data instanceof StreamableFile) {
           return data;
         }
--- src/modules/midjourney/index.controller.ts
+++ src/modules/midjourney/index.controller.ts
@@ -2,11 +2,11 @@
  * @Author: jie.q qiujieee_empty@outlook.com
  * @Date: 2023-05-05 10:22:36
  * @LastEditors: qiujie qiujieee_empty@outlook.com
- * @LastEditTime: 2023-05-23 17:32:11
+ * @LastEditTime: 2023-05-23 20:56:55
  * @FilePath: /yuque_hook/src/core/controllers/hook.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Controller, Post, Body, Logger } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 
 @Controller('/midjourney')
@@ -14,8 +14,14 @@ import { ApiExcludeController } from '@nestjs/swagger';
 export class MidjourneyController {
   private readonly logger = new Logger(MidjourneyController.name);
 
+  @Get('test')
+  test() {
+    return 'test success';
+  }
+
   @Post('hook')
   hook(@Body() payload) {
     this.logger.log(JSON.stringify(payload));
+    return true;
   }
 }
